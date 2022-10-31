14df7c1908306ca3193f955a55a99c05af70546a
bcardona bcardona@PSLT6260.personalsoft.com <bcardona bcardona@PSLT6260.personalsoft.com>
2022-10-31 18:31:26 -0500

    Archivos routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { ChevroletComponent } from './chevrolet/chevrolet.component';
-import { ToyotaComponent } from './toyota/toyota.component';
+
+import { ChevroletComponent } from './cars/chevrolet/chevrolet.component'; 
+import { ToyotaComponent } from './cars/toyota/toyota.component'; 
+import { RenaultComponent } from './cars/renault/renault.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ChevroletComponent,
-    ToyotaComponent
+    ToyotaComponent,
+    RenaultComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
